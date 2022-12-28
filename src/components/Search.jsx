98692b8f53b9a31ceefdd4f0e36f3cefe6e50b8c
Kinzume <81969626+Kinzume@users.noreply.github.com>
2022-12-28 21:14:10 +0000

    feat: update search with mui5

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,6 +2,11 @@ import React, { useCallback, useRef, useState } from "react";
 import "../styles/Search.css";
 import useCharSearch from "./useCharSearch";
 import SearchData from "../data/SearchData.json";
+import TextField from "@mui/material/TextField";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import { Avatar, Button } from "@mui/material";
 
 export default function Search() {
   const [query, setQuery] = useState("Leo");
@@ -37,17 +42,45 @@ export default function Search() {
 
   return (
     <article className="search">
-      <input
+      <form action="">
+        <TextField
+          id="outlined-basic"
+          label="Player Character"
+          variant="outlined"
+          value={query}
+          onChange={handleSearch}
+        />
+        <Button>Search</Button>
+      </form>
+      <List>
+        {SearchData?.Results?.map((character) => (
+          <ListItem
+            onClick={() => console.log(character?.ID)}
+            key={character?.ID}
+          >
+            <ListItemText
+              primary={character?.Name}
+              secondary={character?.Server}
+            />
+            <Avatar
+              alt={character?.Name}
+              src={character?.Avatar}
+            />
+          </ListItem>
+        ))}
+      </List>
+
+      {/* <input
         value={query}
         type="text"
         name=""
         id=""
         onChange={handleSearch}
-      />
+      /> 
       {SearchData?.Results?.map((character) => (
         <p key={character?.ID}>{character?.Name}</p>
       ))}
-      {/*  <input
+ <input
         value={query}
         type="text"
         name=""
